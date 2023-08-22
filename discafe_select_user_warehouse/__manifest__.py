aa5e8e3e7487848efe226cd0fc2570f7b85f30b5
Dani <dani.xtendoo@gmail.com>
2023-08-22 11:54:22 +0200

    [ADD] discafe_select_user_warehouse

---
+++ discafe_select_user_warehouse/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Discafe Select User Warehouse',
+    'summary': """Discafe Select User Warehouse""",
+    'version': '12.0.1.0.0',
+    'description': """Discafe Select User Warehouse""",
+    'author': 'DDL',
+    'company': 'Xtendoo',
+    'website': 'http://www.xtendoo.es',
+    'category': 'Extra Tools',
+    'depends': [
+        'select_user_warehouse',
+        'stock',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'security/security.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+
+}
