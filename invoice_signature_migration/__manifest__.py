da3d1b7b2eb8488866ddd3725d89bfbf8928f0af
dani <dani.xtendoo@gmail.com>
2025-03-28 08:32:31 +0100

    [MOD] document_format_dis, arreglados los demas documentos

---
+++ invoice_signature_migration/__manifest__.py
@@ -0,0 +1,14 @@
+# __manifest__.py
+{
+    'name': 'Módulo Básico de Ejemplo',
+    'version': '13.0.1.0.0',
+    'category': 'Tools',
+    'description': 'Un módulo básico para probar el post_init_hook.',
+    'author': 'Tu nombre',
+    'website': 'http://www.tuempresa.com',
+    'depends': [],
+    'data': [],
+    'post_init_hook': '_post_init_hook',  # Hook de instalación
+    'installable': True,
+    'application': True,
+}
