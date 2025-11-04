563083c558857c924554fc60c01c9867411badd4
Guillermo Bárcena López <barcenalopezguillermo@gmail.com>
2025-11-04 10:36:36 +0100

    migrated

---
+++ res_partner_show_account/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Res Partner show account',
+    'summary': """Hides the phone field and shows internal reference and accounting accounts""",
+    'version': '18.0.1.0.0',
+    'description': """Hides the phone field and shows internal reference and accounting accounts""",
+    'author': 'DDL',
+    'company': 'Xtendoo',
+    'website': 'http://www.xtendoo.es',
+    'category': 'Extra Tools',
+    'depends': [
+        'base',
+        'account',
+    ],
+    'license': 'AGPL-3',
+    'data': [
+        'views/res_partner_show_account.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
