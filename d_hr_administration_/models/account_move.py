8ef0ef3d1e58d7d1afc720af27df583a4b4ad370
Guillermo Bárcena López <barcenalopezguillermo@gmail.com>
2025-11-05 10:08:59 +0100

    view invoices

--- d_hr_administration_/models/account_move.py
+++ d_hr_administration_/models/account_move.py
@@ -11,26 +11,27 @@ class AccountMove(models.Model):
     _name = 'account.move'
     _inherit = ['account.move', 'administrator.mixin.rule']
 
-    @api.model
-    def default_get(self, default_fields):
+    @api.model_create_multi
+    def create(self, vals_list):
         """
-        Si el contexto trae el dato 'active_model' y ese model es 'sale_order' 
-        eso quiere decir que viene de un pedido por tanto lo dejamos pasar
+        Bloquear la creación de facturas excepto si:
+        - El usuario es administrador
+        - La factura viene de un pedido de venta
         """
         # Check if user is administrator
         if self.env.user.has_group('d_hr_administration.administration'):
-            return super().default_get(default_fields)
-        
+            return super().create(vals_list)
+
         # Check if coming from sale order
         context_params = self.env.context.get('params', {})
         if context_params.get('model') == 'sale.order' or context_params.get('is_sale'):
-            return super().default_get(default_fields)
-        
+            return super().create(vals_list)
+
         # Check if it's coming from active_model context
         if self.env.context.get('active_model') == 'sale.order':
-            return super().default_get(default_fields)
-            
-        raise ValidationError("You are not allowed to create invoices.")
+            return super().create(vals_list)
+
+        raise ValidationError("No tiene permisos para crear facturas manualmente. Use un pedido de venta.")
 
     def action_cancel(self):
         """Override cancel action to check permissions"""
