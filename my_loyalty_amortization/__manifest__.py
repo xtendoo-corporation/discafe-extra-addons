b9d4b38e6b11af2becbbb00538a5d153702e332b
Guillermo Bárcena López <barcenalopezguillermo@gmail.com>
2025-11-11 07:23:42 +0100

    sync

---
+++ my_loyalty_amortization/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Loyalty Program Amortization',
+    'version': '18.0.1.0.0',
+    'category': 'Sales/Loyalty',
+    'summary': 'Agrega campo de amortización a programas de fidelidad Buy X Get Y',
+    'description': """
+        Este módulo extiende los programas de fidelidad de Odoo 18 para agregar
+        un campo booleano "Amortización" que solo es visible cuando el tipo de
+        promoción es "Comprar X recibir Y" (buy_x_get_y).
+    """,
+    'author': 'Guillermo Bárcena López',
+    'website': 'https://www.discafe.com',
+    'depends': [
+        'base',
+        'sale',
+        'loyalty',
+    ],
+    'data': [
+        'views/loyalty_program_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
+
