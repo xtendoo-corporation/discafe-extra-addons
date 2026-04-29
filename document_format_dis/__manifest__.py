4d917f46ed0f57557382eebf35e0359a10b62ac3
dani <dani.xtendoo@gmail.com>
2026-04-29 13:49:23 +0200

    sync

--- document_format_dis/__manifest__.py
+++ document_format_dis/__manifest__.py
@@ -13,11 +13,47 @@
         'base',
         'account',
         'sale',
+        'sale_stock',
+        'purchase',
+        'purchase_stock',
         'web',
-        'stock'
+        'stock',
+        'l10n_es_partner',
+        'partner_delivery_zone',
     ],
     'license': 'AGPL-3',
     'data': [
+
+        # Comentario migración Odoo 18: se recuperan también las vistas PDF normales.
+
+        # Ventas PDF
+        'views/sale/report_saleorder_document_without_promotions.xml',
+        'views/sale/report_saleorder_document_promotions.xml',
+        'views/sale/saleorder_promotions.xml',
+
+        # Albaranes PDF
+        'views/delivery/report_delivery_document_without_promotions.xml',
+        'views/delivery/report_delivery_document_promotions.xml',
+        'views/delivery/delivery_promotions.xml',
+
+        # Informe de carga PDF
+        'views/workload/stock_picking_report.xml',
+
+        # Facturas PDF
+        'views/invoice/report_invoice_document_without_promotions.xml',
+        'views/invoice/report_invoice_document_promotions.xml',
+        'views/invoice/invoice_promotions.xml',
+        'views/invoice_with_description/report_invoice_document_with_description.xml',
+        'views/invoice_with_description/invoice_with_description.xml',
+        'views/invoice_without_promotions/invoice_without_promotions.xml',
+
+        # Pagos PDF
+        'views/payment/report_payment_receipt.xml',
+
+        # Compras PDF
+        'views/purchase/report_purchase_document.xml',
+
+        'views/layout/external_layout_clean.xml',
 
         # Sólo vistas Bluetooth (se han eliminado las vistas "normales")
 
